2f52085ca81ea36cb7c66d114b21ccd4d9f9f1f6
hanzeduardodedios1 <hanzeduardo@csu.fullerton.edu>
2026-01-01 14:09:51 -0800

    feat: setup Riot service and implement get_puuid function

---
+++ backend/services/riot_service.py
@@ -0,0 +1,34 @@
+import os
+import requests
+import urllib.parse
+from dotenv import load_dotenv
+
+#load the .env file
+load_dotenv()
+
+#Get API key safely
+#Goes to the .env file and retrieves 'RIOT-API-KEY' value
+API_KEY = os.getenv("RIOT-API-KEY")
+
+#Safety check
+if not API_KEY:
+    raise ValueError("No API Key found.")
+
+headers = {
+    "X-Riot-Token: ": API_KEY
+} #"X-Riot-Token" is the SPECIFIC HEADER for RIOT servers
+
+# ----------------------------------------------------------------------------------------------------------
+# 1. Getting the PUUID (Using user's RIOT ID is ineffective because it can be changed. Their PUUID is const)
+# ----------------------------------------------------------------------------------------------------------
+def get_puuid(game_name, tag_line):
+    #"americas" - the routing region for NA/BR/LAN/LAS
+    url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return requests.json().get("puuid")
+    return None
+
+
+
+
